bd91515431ad9ec1d45537b8c8b18435e9e4f2dc
davisstacked <brettzvi@gmail.com>
2021-09-22 14:50:38 -0700

    counter functions

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -19,8 +19,16 @@ export class CoinFlipper extends Component {
 
   flipCoin() {
     const random = Math.floor(Math.random() * 2);
-    random === 0 ? this.setState({ heads: true }) : this.setState({ heads: false });
+    random === 0
+      ? this.setState({ heads: true }) : this.setState({ heads: false });
+    this.setState(prevState => ({ numOfFlips: prevState.numOfFlips + 1 }));
+    random === 0
+      ? this.setState(prevState => ({ numOfHeads: prevState.numOfHeads + 1 }))
+      : this.setState(prevState => ({ numOfTails: prevState.numOfTails + 1 }));
+      console.log(random);
   };
+
+  
 
   handleClick() {
     this.flipCoin();
@@ -33,7 +41,7 @@ export class CoinFlipper extends Component {
         <h1>Let's flip a coin!</h1>
         <Coin heads={this.state.heads} />
         <button onClick={this.handleClick}>Flip Me!</button>
-        {/* <p>Out of {this.state.numOfFlips}, there have been {this.state.numOfHeads} heads and {this.state.numOfTails} tails.</p> */}
+        <p>Out of {this.state.numOfFlips}, there have been {this.state.numOfHeads} heads and {this.state.numOfTails} tails.</p>
       </div>
     )
   }
